e871640f1c981775335d531358d7a89d199d7fb5
Syamala Maddina <syamalamaddina@gmail.com>
2025-05-17 17:58:50 -0400

    code files are ready with code

---
+++ agents.py
@@ -0,0 +1,9 @@
+import os
+from autogen import AssistantAgent
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+class ResearchAgents:
+    pass
---
+++ data_loader.py
@@ -0,0 +1,8 @@
+import requests
+import xml.etree.ElementTree as ET
+from scholarly import scholarly
+
+print("ok")
+
+class DataLoader:
+    pass
